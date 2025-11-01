4f8ebe112bd368a2574d03e9f4b432cfac1520e4
cbianco <bnbiancocristian@gmail.com>
2025-11-01 16:15:35 +0100

    Refactor main method to initialize options from environment variables

--- src/main/java/dev/dstruct/Main.java
+++ src/main/java/dev/dstruct/Main.java
@@ -1,12 +1,8 @@
 package dev.dstruct;
 
-import dev.dstruct.wal.SyncPolicy;
-
 public class Main {
 	static void main(String[] args) throws Exception {
-		Options options = new Options();
-		options.writeAHeadLogging = false;
-		options.syncPolicy = SyncPolicy.ASYNC;
+		Options options = Options.fromEnv();
 		DStruct dStruct = new DStruct(options);
 		dStruct.start();
 	}
